b306455d7dc7a1ad8740276722ed533e9fcee2eb
Lukasz Bajor <lukaszic@o2.pl>
2016-04-19 00:53:40 +0200

    Rest WS now mapped into proper search path

--- src/main/java/bajor/lukasz/controller/StationController.java
+++ src/main/java/bajor/lukasz/controller/StationController.java
@@ -1,17 +1,18 @@
 package bajor.lukasz.controller;
 
 import bajor.lukasz.model.Station;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by lbajor on 2016-04-19.
  */
 @RestController
+@RequestMapping("/stations")
 public class StationController {
-    @RequestMapping("/stations")
-    public Station station(@RequestParam(value="name") String name) {
+    @RequestMapping("/search/{name}")
+    public Station searchStation(@PathVariable String name) {
         return new Station(1, "Test");
     }
 }
